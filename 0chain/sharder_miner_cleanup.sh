718677aab753a92150257b02b48e29ef467eccf8
shahnawaz-creator <msac8225@gmail.com>
2023-12-23 10:40:39 +0100

    fix cleanup script

--- 0chain/sharder_miner_cleanup.sh
+++ 0chain/sharder_miner_cleanup.sh
@@ -7,17 +7,17 @@ export PROJECT_ROOT=/var/0chain # /var/0chain
 echo -e "\e[32m Successfully Created \e[23m \e[0;37m"
 
 echo -e "\n\e[93m===============================================================================================================================================================================
-                                                                        Checking Miner counts.
+                                                                        cleaning up sharder/miner.
 ===============================================================================================================================================================================  \e[39m"
 pushd ${PROJECT_ROOT} > /dev/null;
     docker rm -f $(docker ps -a -q)
-    rm -rf * /var/0chain/miner/ssd/* || true
-    rm -rf * /var/0chain/miner/hdd/* || true
-    rm -rf * /var/0chain/grafana-portainer/* || true
-    rm -rf * /var/0chain/sharder/ssd/* || true
-    rm -rf * /var/0chain/sharder/hdd/* || true
-    rm -rf * /var/0chain/*.zip || true
-    rm -rf * /var/0chain/initial_states.yaml || true
+    rm -rf /var/0chain/miner/ssd/* || true
+    rm -rf /var/0chain/miner/hdd/* || true
+    rm -rf /var/0chain/grafana-portainer/* || true
+    rm -rf /var/0chain/sharder/ssd/* || true
+    rm -rf /var/0chain/sharder/hdd/* || true
+    rm -rf /var/0chain/*.zip || true
+    rm -rf /var/0chain/initial_states.yaml || true
     echo 'y' | docker system prune -a || true
     echo 'y' | docker volume prune -a || true
 popd > /dev/null;
