981f207f10dda19c6d144fcf72ee8dc120b2d1f5
Manohar Reddy <b.manu199@gmail.com>
2023-12-29 13:06:01 +0100

    use tag: v1.11.5 for s3mgrt

--- migration.sh
+++ migration.sh
@@ -44,7 +44,7 @@ fi
 
 # docker image
 DOCKER_TAG=v1.11.0
-S3MGRT_AGENT_TAG=staging
+S3MGRT_AGENT_TAG=v1.11.5
 
 sudo apt update
 DEBIAN_FRONTEND=noninteractive sudo apt install -y unzip curl containerd docker.io jq net-tools
