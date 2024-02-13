7a8ca1046fc86df23ad166475cb1c4ff99cd1e9d
shahnawaz-creator <117025384+shahnawaz-creator@users.noreply.github.com>
2024-02-13 20:25:50 +0530

    Updated snapshot_recovery.sh to latest snapshot

--- 0chain/snapshot_recovery.sh
+++ 0chain/snapshot_recovery.sh
@@ -6,7 +6,7 @@ docker rm -f sharder-1 sharder-postgres-1
 # Removing old data
 cd /var/0chain/sharder/hdd/docker.local/sharder1/
 if [ ! -d "./data_backup" ]; then
-    mv data data_backup
+    mv data data_backup-11-feb
 else
     rm -rf data
 fi
@@ -24,13 +24,13 @@ cd /var/0chain/sharder/hdd
 mkdir snapshot || true
 cd snapshot
 
-wget https://zus-snapshots.s3.amazonaws.com/sharder4_0chain_net/sharder-blocks.tar.gz
-wget https://zus-snapshots.s3.amazonaws.com/sharder4_0chain_net/sharder-mpt.tar.gz
-wget https://zus-snapshots.s3.amazonaws.com/sharder4_0chain_net/sharder-sql2.tar.gz
-wget https://zus-snapshots.s3.amazonaws.com/sharder4_0chain_net/sharder-ssd-sql.tar.gz
+wget https://zus-snapshots.s3.amazonaws.com/sharder4_0chain_net/sharder-blocks-11-Feb.tar.gz
+wget https://zus-snapshots.s3.amazonaws.com/sharder4_0chain_net/sharder-mpt-11-Feb.tar.gz
+wget https://zus-snapshots.s3.amazonaws.com/sharder4_0chain_net/sharder-sql2-11-Feb.tar.gz
+wget https://zus-snapshots.s3.amazonaws.com/sharder4_0chain_net/sharder-ssd-sql-11-Feb.tar.gz
 
 # extract sharder-blocks.tar.gz
-tar -xzvf sharder-blocks.tar.gz
+tar -xzvf sharder-blocks-11-Feb.tar.gz
 
 # Find all .tar.gz files in sharder_blocks and its subdirectories
 find sharder-blocks -type f -name "*.tar.gz" -print0 | while IFS= read -r -d '' file; do
@@ -39,15 +39,15 @@ find sharder-blocks -type f -name "*.tar.gz" -print0 | while IFS= read -r -d '' 
 done
 
 # extract sharder-mpt.tar.gz to path /var/0chain/sharder/hdd/docker.local/sharder1/data/
-tar -zxvf sharder-mpt.tar.gz -C /
+tar -zxvf sharder-mpt-11-Feb.tar.gz -C /
 
 # extract sharder-ssd-sql.tar.gz /var/0chain/sharder/ssd/docker.local/sharder1/data/
-tar -zxvf sharder-ssd-sql.tar.gz -C /
+tar -zxvf sharder-ssd-sql-11-Feb.tar.gz -C /
 
 # extract sharder-sql2.tar.gz /var/0chain/sharder/hdd/docker.local/sharder1/data/
-tar -zxvf sharder-sql2.tar.gz -C /
+tar -zxvf sharder-sql2-11-Feb.tar.gz -C /
 
 # Starting Sharder with snapshot data
-yq e -i ".services.sharder.image = \"0chaindev/sharder:v1.11.8\"" /var/0chain/sharder/ssd/docker.local/build.sharder/p0docker-compose.yaml
+yq e -i ".services.sharder.image = \"0chaindev/sharder:staging-5cfd26f3\"" /var/0chain/sharder/ssd/docker.local/build.sharder/p0docker-compose.yaml
 cd /var/0chain/sharder/ssd/docker.local/sharder1/
 sudo bash ../bin/start.p0sharder.sh /var/0chain/sharder/ssd /var/0chain/sharder/hdd/
