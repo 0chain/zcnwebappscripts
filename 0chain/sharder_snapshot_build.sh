0716c5de62c18ff7a0fea1bf17a612dd738d23d8
shahnawaz-creator <msac8225@gmail.com>
2024-03-21 15:03:17 +0100

    updated sharder snapshot script

--- 0chain/sharder_snapshot_build.sh
+++ 0chain/sharder_snapshot_build.sh
@@ -53,10 +53,34 @@ echo -e "\n\e[93m===============================================================
 ===============================================================================================================================================================================  \e[39m"
 docker start sharder-1 sharder-postgres-1
 
-# move these zip file to s3 bucket zus-snapshots/<sharder-snapshot-folder>
+# echo -e "\n\e[93m===============================================================================================================================================================================
+#                                                                             Copy the snapshot wallet to location ~/snapshots/snapshot.json path.
+# ===============================================================================================================================================================================  \e[39m"
+# read -p "Press enter after placing snapshot.json wallet to path ~/snapshot/"
+
+# move these zip file to zus blobber storage zus-snapshots/<sharder-snapshot-folder>
 echo -e "\n\e[93m===============================================================================================================================================================================
-                                                                            Moving snapshot files to s3.
+                                                                            Moving snapshot files to zus storage.
 ===============================================================================================================================================================================  \e[39m"
+# wget https://github.com/0chain/zcnwebappscripts/raw/as-deploy/0chain/zwallet-binary/zbox
+# chmod +x zbox
+
+# echo "Generating config.yaml file"
+# echo "block_worker: https://mainnet.zus.network/dns" > config.yaml
+# echo "signature_scheme: bls0chain" >> config.yaml
+# echo "min_submit: 20" >> config.yaml
+# echo "min_confirmation: 20" >> config.yaml
+# echo "confirmation_chain_length: 3" >> config.yaml
+# echo "max_txn_query: 5" >> config.yaml
+# echo "query_sleep_time: 5" >> config.yaml
+
+# echo "Generating snapshot.json file"
+# echo '{"client_id":"e1373e3d129b8d125549ec2527d8515eff7b9b02e6094dff1fe6545b62058041","client_key":"560df578de5a224ac779a8e8e56c469243141171370c59b74a35b994df54c10c411107e97b350a6af9428d63ef42f54b2991880aa2205c7869909534efdab611","keys":[{"public_key":"560df578de5a224ac779a8e8e56c469243141171370c59b74a35b994df54c10c411107e97b350a6af9428d63ef42f54b2991880aa2205c7869909534efdab611","private_key":"94bed72f76cc48517e2f3b5386d0072d4d5c7e88e20300cc9d2385e51a235c01"}],"mnemonics":"auto icon flight enemy culture three field track album kiss accuse weather member diagram symbol where tank doll naive space injury problem blade universe","version":"1.0","date_created":"2024-03-21T12:56:02+01:00","nonce":0}' > snapshot.json
+
+# ./zbox upload --localpath ./sharder-sql2-${SNAP_VERSION}.tar.gz --remotepath /${SHARDER_SNAP}/sharder-sql2-${SNAP_VERSION}.tar.gz --allocation a25cde7d0b06655f4f8eb86ec99050eee0b6d929161c62551456acc276adab63 --configDir . --config ./config.yaml --wallet snapshot.json --silent
+# ./zbox upload --localpath ./sharder-ssd-sql-${SNAP_VERSION}.tar.gz --remotepath /${SHARDER_SNAP}/sharder-ssd-sql-${SNAP_VERSION}.tar.gz --allocation a25cde7d0b06655f4f8eb86ec99050eee0b6d929161c62551456acc276adab63 --configDir . --config ./config.yaml --wallet snapshot.json --silent
+# ./zbox upload --localpath ./sharder-mpt-${SNAP_VERSION}.tar.gz --remotepath /${SHARDER_SNAP}/sharder-mpt-${SNAP_VERSION}.tar.gz --allocation a25cde7d0b06655f4f8eb86ec99050eee0b6d929161c62551456acc276adab63 --configDir . --config ./config.yaml --wallet snapshot.json --silent
+
 # aws s3 cp sharder-blocks-${SNAP_VERSION}.tar.gz s3://zus-snapshots/${SHARDER_SNAP}/
 aws s3 cp sharder-sql2-${SNAP_VERSION}.tar.gz s3://zus-snapshots/${SHARDER_SNAP}/
 aws s3 cp sharder-ssd-sql-${SNAP_VERSION}.tar.gz s3://zus-snapshots/${SHARDER_SNAP}/
@@ -66,3 +90,4 @@ echo -e "\n\e[93m===============================================================
                                                                             Link to docs to deploy sharder snapshot.
 ===============================================================================================================================================================================  \e[39m"
 echo "Follow docs to deploy snapshot to bad sharder --> https://0chaindocs.gitbook.io/as-onboarding/recovery-from-snapshots/steps-to-apply-snapshot"
+
--- 0chain/sharder_snapshot_recovery.sh
+++ 0chain/sharder_snapshot_recovery.sh
@@ -36,7 +36,7 @@ echo "Backup recent ssd postgres data"
 mv postgresql postgresql_bkp || true
 
 echo -e "\n\e[93m===============================================================================================================================================================================
-                                                                            Downloading latest snapshot from the server.
+                                                                            Downloading latest snapshot from the zus storage.
 ===============================================================================================================================================================================  \e[39m"
 # Downloading snapshot
 cd /var/0chain/sharder/hdd
@@ -45,7 +45,26 @@ cd snapshot
 echo "Removing previous pulled snapshot if exists"
 rm -rf ./*
 
-echo "Downloading new snapshot"
+# echo "Downloading new snapshot"
+# wget https://github.com/0chain/zcnwebappscripts/raw/as-deploy/0chain/zwallet-binary/zbox
+# chmod +x zbox
+
+# echo "Generating config.yaml file"
+# echo "block_worker: https://mainnet.zus.network/dns" > config.yaml
+# echo "signature_scheme: bls0chain" >> config.yaml
+# echo "min_submit: 20" >> config.yaml
+# echo "min_confirmation: 20" >> config.yaml
+# echo "confirmation_chain_length: 3" >> config.yaml
+# echo "max_txn_query: 5" >> config.yaml
+# echo "query_sleep_time: 5" >> config.yaml
+
+# echo "Generating snapshot.json file"
+# echo '{"client_id":"e1373e3d129b8d125549ec2527d8515eff7b9b02e6094dff1fe6545b62058041","client_key":"560df578de5a224ac779a8e8e56c469243141171370c59b74a35b994df54c10c411107e97b350a6af9428d63ef42f54b2991880aa2205c7869909534efdab611","keys":[{"public_key":"560df578de5a224ac779a8e8e56c469243141171370c59b74a35b994df54c10c411107e97b350a6af9428d63ef42f54b2991880aa2205c7869909534efdab611","private_key":"94bed72f76cc48517e2f3b5386d0072d4d5c7e88e20300cc9d2385e51a235c01"}],"mnemonics":"auto icon flight enemy culture three field track album kiss accuse weather member diagram symbol where tank doll naive space injury problem blade universe","version":"1.0","date_created":"2024-03-21T12:56:02+01:00","nonce":0}' > snapshot.json
+
+# ./zbox download --remotepath /${SHARDER_SNAP}/sharder-sql2-${SNAP_VERSION}.tar.gz --localpath ./sharder-sql2-${SNAP_VERSION}.tar.gz --allocation a25cde7d0b06655f4f8eb86ec99050eee0b6d929161c62551456acc276adab63 --configDir . --config ./config.yaml --wallet snapshot.json --slient
+# ./zbox download --remotepath /${SHARDER_SNAP}/sharder-ssd-sql-${SNAP_VERSION}.tar.gz --localpath ./sharder-ssd-sql-${SNAP_VERSION}.tar.gz --allocation a25cde7d0b06655f4f8eb86ec99050eee0b6d929161c62551456acc276adab63 --configDir . --config ./config.yaml --wallet snapshot.json --slient
+# ./zbox download --remotepath /${SHARDER_SNAP}/sharder-mpt-${SNAP_VERSION}.tar.gz --localpath ./sharder-mpt-${SNAP_VERSION}.tar.gz --allocation a25cde7d0b06655f4f8eb86ec99050eee0b6d929161c62551456acc276adab63 --configDir . --config ./config.yaml --wallet snapshot.json --slient
+
 # wget https://zus-snapshots.s3.amazonaws.com/${SHARDER_SNAP}/sharder-blocks-${SNAP_VERSION}.tar.gz
 wget https://zus-snapshots.s3.amazonaws.com/${SHARDER_SNAP}/sharder-mpt-${SNAP_VERSION}.tar.gz
 wget https://zus-snapshots.s3.amazonaws.com/${SHARDER_SNAP}/sharder-sql2-${SNAP_VERSION}.tar.gz
